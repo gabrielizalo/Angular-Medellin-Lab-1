f6731f04e661b746c79d6bb5eae12820d8795f98
Gabriel Porras <ghporras@gmail.com>
2018-02-10 11:13:02 -0500

    First Release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My Angular App';
+  title = 'Yes or Not?';
+  response;
+
+  constructor(private http: HttpClient) {
+    http.get('https://yesno.wtf/api').subscribe((response) => this.response = response);
+  }
 }
